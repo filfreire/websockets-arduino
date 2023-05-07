e33f7dc540118fa059dddf541664650233415726
Filipe Freire <livrofubia@gmail.com>
2023-05-07 19:47:40 +0100

    led blick using websockets

--- README.md
+++ README.md
@@ -12,8 +12,25 @@ node ledtest.js
 
 ![example screenshot](example.jpg)
 
+Connect to `ws://localhost:3000`, and send:
+
+```json
+{
+    "led":on
+}
+```
+
+![insomnia example websocket connection](insomnia-led.jpg)
+
+![example of led on, arduino](led-arduino.jpg)
+
 ### Windows problems
 
 - Install Python and Visual Studio (with C++ libs)
 - Install `node-gyp` - `npm install -g node-gyp`
 - Install `serialport` - `npm install serialport`
+
+## See also
+
+- [https://www.instructables.com/Javascript-robotics-and-browser-based-Arduino-cont/]
+- [https://www.makeuseof.com/tag/control-arduino-using-javascript/]
--- ledtest.js
+++ ledtest.js
@@ -1,9 +1,36 @@
 const { Board, Led } = require("johnny-five");
+const { WebSocketServer } = require("ws");
 const board = new Board();
 
+// Create an Led on pin 13
+let led;
+
 board.on("ready", () => {
-  // Create an Led on pin 13
-  const led = new Led(13);
+  led = new Led(13).off();
   // Blink every half second
-  led.blink(500);
+  // led.blink(500);
+});
+
+
+const wss = new WebSocketServer({ port: 3000 });
+
+wss.on('connection', function connection(ws) {
+  ws.on('error', console.error);
+
+  ws.on('message', function message(data) {
+    const parsedData = JSON.parse(data);
+    if (board.isReady) {
+      if (parsedData.led == "on") { // on
+        ws.send('led:on');
+        led.on();
+      }
+
+      if (parsedData.led == "off") { // off
+        ws.send('led:off');
+        led.off();
+      }
+    }
+  });
+
+  ws.send('something');
 });

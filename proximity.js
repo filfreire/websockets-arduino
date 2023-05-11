376d4b1c5bdee61c222f3199cd9ea78fe1f7ccce
Filipe Freire <livrofubia@gmail.com>
2023-05-11 01:11:54 +0100

    weird problem on reconnect/refresh

--- proximity.js
+++ proximity.js
@@ -1,49 +1,59 @@
 const { Board, Proximity, Led } = require("johnny-five");
-const { WebSocketServer } = require("ws");
+const { WebSocket, WebSocketServer } = require("ws");
 const board = new Board();
-let proximity, led;
-
-const wss = new WebSocketServer({ port: 3000 });
-
-board.on("ready", () => {
-    proximity = new Proximity({
-        controller: "HCSR04",
-        pin: "A0"
-    });
-
-    led = new Led(13).off();
-});
+let led;
 
 let distanceThresholdReached = false;
 const DISTANCE_THRESHOLD = 50;
 
 const DEBUG = false;
 
-wss.on('connection', function connection(ws) {
-    ws.on('error', console.error);
+const wss = new WebSocketServer({ port: 3000 });
 
-    proximity.on("change", () => {
-        const { centimeters } = proximity;
+function sendProximityEvents(client, proximity) {
+    const { centimeters } = proximity;
 
-        if (centimeters <= DISTANCE_THRESHOLD && !distanceThresholdReached) {
-            console.log('DISTANCE_THRESHOLD_REACHED');
-            ws.send('DISTANCE_THRESHOLD_REACHED');
-            distanceThresholdReached = true;
-            led.on();
-        }
-        else if (centimeters > DISTANCE_THRESHOLD) {
-            distanceThresholdReached = false;
-            led.off()
-        }
-        if (DEBUG) {
-            ws.send(JSON.stringify({ 'debug-distance': centimeters }));
-            console.log("Proximity  cm  : ", centimeters);
-        }
+    if (centimeters <= DISTANCE_THRESHOLD && !distanceThresholdReached) {
+        console.log('DISTANCE_THRESHOLD_REACHED');
+        client.send('DISTANCE_THRESHOLD_REACHED');
+        distanceThresholdReached = true;
+        led.on();
+    }
+    else if (centimeters > DISTANCE_THRESHOLD) {
+        distanceThresholdReached = false;
+        led.off()
+    }
+    if (DEBUG) {
+        client.send(JSON.stringify({ 'debug-distance': centimeters }));
+        console.log("Proximity  cm  : ", centimeters);
+    }
 
+}
+
+board.on("ready", () => {
+
+    led = new Led(13).off();
+    let proximity = new Proximity({
+        controller: "HCSR04",
+        pin: "A0"
     });
 
-    ws.on('close', function close() {
-        console.log('disconnected');
-    });
+    wss.on('connection', function connection(ws) {
+        ws.on('error', console.error);
 
+
+        wss.clients.forEach(function each(client) {
+            if (client.readyState === WebSocket.OPEN && board.isReady) {
+                console.log('open!');
+                proximity.on("change", () => sendProximityEvents(client, proximity));
+
+                client.on('close', function close() {
+                    console.log('disconnected');
+                    // TODO remove listener not working in subsequent ws connections
+                    // proximity.removeListener("change", sendProximityEvents)
+                });
+            }
+        });
+
+    });
 });
